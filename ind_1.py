2c03f5a1447217d35e7d2bd0d66f168ef8671f27
Belyaev126 <belyaevrw@gmail.com>
2020-12-05 17:39:55 +0300

    Индивидуальное задание 1

---
+++ ind_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- config: utf-8 -*-
+
+# Вариант 11. Написать программу, которая считывает текст из файла и выводит на экран только
+# предложения, начинающиеся с тире, перед которым могут находиться только пробельные
+# символы.
+
+if __name__ == '__main__':
+    with open('text.txt', 'r')as f:
+        text = f.read()
+
+        text = text.replace("!", ".")
+        text = text.replace("?", ".")
+
+        while ".." in text:
+            text.replace("..", ".")
+
+        sentences = text.split(".")
+
+        for sentence in sentences:
+            if "-" in sentence:
+                with open('text.txt', 'r')as s:
+                    f_text = s.read()
+                    if sentence in f_text:
+                        print(f'{sentence}{f_text[f_text.rfind(sentence)+len(sentence)]}')
+
