30932969ce3af7bdcebb78ea4d700daf86d66cb1
Belyaev126 <70748554+Belyaev126@users.noreply.github.com>
2020-12-11 11:55:38 +0300

    Update mod_1.py

--- mod_1.py
+++ mod_1.py
@@ -14,8 +14,6 @@ if __name__ == '__main__':
         sentences = text.split(".")
 
         for sentence in sentences:
-            if "," in sentence:
-                with open('text.txt', 'r')as s:
-                    f_text = s.read()
-                    if sentence in f_text:
-                        print(f'{sentence}{f_text[f_text.rfind(sentence)+len(sentence)]}')
+            if "," in sentences
+                    if sentence in text:
+                        print(sentence)
